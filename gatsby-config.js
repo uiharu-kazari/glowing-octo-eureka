b57eb7dd2cf518c1c0ab24baabd56bbe7d1aaf06
kasumi <xucgphi@gmail.com>
2022-07-29 11:21:27 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,21 @@ module.exports = {
   },
   plugins: ["gatsby-plugin-image", 
   "gatsby-plugin-sharp", 
-  "gatsby-transformer-sharp", {
+  "gatsby-transformer-sharp", 
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+  },
+]
 };
